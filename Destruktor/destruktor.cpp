ad2c9b50273002c68380a5a724ee789f3ec7dc7f
Naufal Dzakwan <naufal.dzakwan.ft19@mail.umy.ac.id>
2023-05-15 18:55:14 +0700

    8th commit

--- Destruktor/destruktor.cpp
+++ Destruktor/destruktor.cpp
@@ -11,3 +11,16 @@ public:
 	void cetakData();
 	void isiData();
 };
+//Definisi member funtion
+angka::angka(int i) { //Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() { //Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
